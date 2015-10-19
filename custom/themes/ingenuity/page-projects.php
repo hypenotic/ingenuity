2dbe1f71325ea77d503494bdeeb15c2aeb4e844a
Emily Dela Cruz <emily@hypenotic.com>
2015-10-19 09:33:54 -0400

    Update: added content to Projects landing page, styles edit

--- custom/themes/ingenuity/page-projects.php
+++ custom/themes/ingenuity/page-projects.php
@@ -23,34 +23,27 @@
 		</div>
 
 		<div class="main-content">
+
+			<?php the_content(); ?>
 			
-			<?php
-
-			/**
-			* Template for Full-Width Project Panels
-			* @author Emily Dela Cruz (Hypenotic)
-			* @example <?php get_template_part( 'template-part-full-width-panels'); ?>
-			* Dependency: custom-post-projects.php, projects.scss
-			* Original Project: Ingenuity (151019)
-			* 
-			*/
-
-				$query = new WP_Query( array( 'post_type' => 'project' ) );
-
-				if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
-
-					<?php // Get custom meta values 
-						$industries = get_post_meta($post->ID,'_industries',true);
-
-						$banner     = get_post_meta( $post->ID, '_banner_image', true );
-						$bannerurl  = wp_get_attachment_image_src( $banner,'banner', true );
-					?>
-
-				<figure class="projects__panel" style="background-image: url('<?php echo $bannerurl[0] ?>');">
-					<a href="<?php the_permalink(); ?>"><h1><?php the_title(); ?></h1></a>
-				</figure>
-
-			<?php endwhile; endif; ?>
+			<div class="projects__panel-container"><?php
+				
+					$query = new WP_Query( array( 'post_type' => 'project' ) );
+				
+					if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
+				
+						<?php // Get custom meta values 
+							$industries = get_post_meta($post->ID,'_industries',true);
+				
+							$banner     = get_post_meta( $post->ID, '_banner_image', true );
+							$bannerurl  = wp_get_attachment_image_src( $banner,'banner', true );
+						?>
+				
+					<figure class="projects__panel" style="background-image: url('<?php echo $bannerurl[0] ?>');">
+						<a href="<?php the_permalink(); ?>"><h1><?php the_title(); ?></h1></a>
+					</figure>
+				
+				<?php endwhile; endif; ?></div>
 
 		</div>
 	
