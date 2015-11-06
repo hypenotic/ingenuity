3bd1eff32effe0039cba1d102a857a0b545244ee
Emily Dela Cruz <emily@hypenotic.com>
2015-11-06 16:14:56 -0500

    Update: project index styles

--- custom/themes/ingenuity/page-projects.php
+++ custom/themes/ingenuity/page-projects.php
@@ -15,14 +15,11 @@
 	    $subheading = get_post_meta( $post->ID, '_banner_subheading', true );
 	?>
 
-		<div class="default-hero" style="background-image: url('<?php echo $bannerurl[0] ?>'); background-size: cover;">
-		    <hgroup>
-		        <?php if($heading) { ?>
-		        	<h1><span class="yellow-highlight"><?php echo $heading; ?></span></h1>
-		        <?php } ?>
-		        <?php if($subheading) { ?>
-		        	<h2><span class="black-highlight"><?php echo $subheading; ?></span></h2>
-		        <?php } ?>
+		<div class="default-hero">
+		    <figure style="background-image: url('<?php echo $bannerurl[0] ?>'); background-size: cover;"></figure>
+		    <hgroup class="animated fadeInDown">
+		        <h1><?php echo $heading; ?></h1>
+		        <h2><?php echo $subheading; ?></h2>
 		    </hgroup>
 		</div>
 
@@ -30,28 +27,29 @@
 
 			<?php the_content(); ?>
 			
-			<div class="projects__panel-container"><?php
-				
-					$query = new WP_Query( array( 'post_type' => 'project' ) );
-				
-					if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
-				
-						<?php // Get custom meta values 
-							$industries = get_post_meta($post->ID,'_industries',true);
-				
-							$banner     = get_post_meta( $post->ID, '_banner_image', true );
-							$bannerurl  = wp_get_attachment_image_src( $banner,'banner', true );
-						?>
-				
-					<a href="<?php the_permalink(); ?>">
-						<figure class="projects__panel" style="background-image: url('<?php echo $bannerurl[0] ?>');">
-							<div class="color-overlay"></div>
-							<h2><?php the_title(); ?></h2>
-						</figure>
-					</a>
-				
-				<?php endwhile; endif; ?></div>
-
+		</div>
+		
+		<div class="projects__panel-container"><?php
+			
+				$query = new WP_Query( array( 'post_type' => 'project' ) );
+			
+				if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
+			
+					<?php // Get custom meta values 
+						$industries = get_post_meta($post->ID,'_industries',true);
+			
+						$banner     = get_post_meta( $post->ID, '_banner_image', true );
+						$bannerurl  = wp_get_attachment_image_src( $banner,'banner', true );
+					?>
+			
+				<a href="<?php the_permalink(); ?>">
+					<figure class="projects__panel" style="background-image: url('<?php echo $bannerurl[0] ?>');">
+						<div class="color-overlay"></div>
+						<h2><?php the_title(); ?></h2>
+					</figure>
+				</a>
+			
+			<?php endwhile; endif; ?>
 		</div>
 	
 	<?php endwhile; else : ?>
