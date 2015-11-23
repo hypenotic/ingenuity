c905cc633d52f0fc96fa8d93127eff9b83a0d8e7
Emily Dela Cruz <emily@hypenotic.com>
2015-11-23 17:41:37 -0500

    Update: sidebar

--- custom/themes/ingenuity/sidebar.php
+++ custom/themes/ingenuity/sidebar.php
@@ -5,21 +5,21 @@ $authors     = get_post_meta( $post->ID, '_authors_authorlist', true );
 
 ?>
 
-	<aside id="left"> 
-		<p>
+<aside id="left"> 
+	<p>
 
-			<span class="bolded-text">Author(s):</span> 
-			<?php if ($authors) { ?>
-				<?php echo $authors; ?>
-			<?php } else { ?>
-				<a href="<?php the_author_link(); ?>"><?php the_author(); ?></a>
-			<?php } ?>
-			
-		</p>
-		<p><span class="bolded-text">Date:</span> <?php the_time( 'M j, Y' ); ?> </p>
-		<p><span class="bolded-text">Category:</span> <?php echo get_the_category_list(); ?></p>
-		<?php if (has_tag()) { ?>
-			<p><span class="bolded-text">Tags:</span>the_tags();</p>
-		<?php }  else {} ?>
-	</aside>
+		<span class="bolded-text">Author(s):</span> 
+		<?php if ($authors) { ?>
+			<?php echo $authors; ?>
+		<?php } else { ?>
+			<a href="<?php the_author_link(); ?>"><?php the_author(); ?></a>
+		<?php } ?>
+		
+	</p>
+	<p><span class="bolded-text">Date:</span> <?php the_time( 'M j, Y' ); ?> </p>
+	<p><span class="bolded-text">Category:</span> <?php echo get_the_category_list(); ?></p>
+	<?php if (has_tag()) { ?>
+		<p><span class="bolded-text">Tags:</span>the_tags();</p>
+	<?php }  else {} ?>
+</aside>
 
