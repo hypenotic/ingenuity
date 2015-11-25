85285a8873da82af6ad2c7d87c520d1fb004b835
Emily Dela Cruz <emily@hypenotic.com>
2015-11-25 10:31:46 -0500

    Update: favicon

--- custom/themes/ingenuity/header.php
+++ custom/themes/ingenuity/header.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+	 <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
 	<title><?php bloginfo('name'); ?></title>
 	<!-- FONTS -->
 	<script>
