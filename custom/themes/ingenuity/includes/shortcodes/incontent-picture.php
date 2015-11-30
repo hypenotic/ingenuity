0e3aa7b2d57adf428dbe5c5dd95a853d2047ee17
Emily Dela Cruz <emily@hypenotic.com>
2015-11-30 09:31:34 -0500

    Update: in-content gallery image for case studies

--- custom/themes/ingenuity/includes/shortcodes/incontent-picture.php
+++ custom/themes/ingenuity/includes/shortcodes/incontent-picture.php
@@ -6,14 +6,18 @@
 function content_incontentpic_shortcode( $atts ) {
 	$output = '';
 	 
-    $pull_quote_atts = shortcode_atts( array(
+    $pic_atts = shortcode_atts( array(
         'imgsrc' => 'url',
         'caption' => 'Insert caption here.',
     ), $atts );
  
-    $output .= '<div class="pullquote">';
-        $output .=  wpautop( wp_kses_post( $pull_quote_atts[ 'quote' ] ) );
-        $output .= '<p class="attribution">' . wp_kses_post( $pull_quote_atts[ 'attribution' ] ) . '</p>';
+    $output .= '<div class="gallery__single">';
+        $output .= '<div class="gallery__single-half gallery-image wow fadeInLeft">';
+            $output .= '<img src="' . wp_kses_post( $pull_quote_atts[ 'imgsrc' ] ) . '" alt="' . the_title() . '- Gallery Photo">' ;
+        $output .= '</div>';
+        $output .= '<div class="gallery__single-half gallery-caption wow fadeInRight">
+            <p>' . wp_kses_post( $pull_quote_atts[ 'caption' ] ) . '</p>'  ;
+        $output .= '</div>';
     $output .= '</div>';
  
     return $output;
