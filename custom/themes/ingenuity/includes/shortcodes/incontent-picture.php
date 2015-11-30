2165b3ecdd66c49b4397992c47e9b32cf0e36636
Emily Dela Cruz <emily@hypenotic.com>
2015-11-30 16:34:45 -0500

    Update: in-content picture fix

--- custom/themes/ingenuity/includes/shortcodes/incontent-picture.php
+++ custom/themes/ingenuity/includes/shortcodes/incontent-picture.php
@@ -13,7 +13,7 @@ function incontentpic_shortcode( $atts ) {
  
     $output .= '<div id="in-content" class="gallery__single">';
         $output .= '<div class="gallery__single-half gallery-image wow fadeInLeft">';
-            $output .= '<img src="' . wp_kses_post( $pic_atts[ 'imgsrc' ] ) . '" alt="' . the_title() . '- Gallery Photo">' ;
+            $output .= '<img src="' . wp_kses_post( $pic_atts[ 'imgsrc' ] ) . '">' ;
         $output .= '</div>';
         $output .= '<div class="gallery__single-half gallery-caption wow fadeInRight">
             <p>' . wp_kses_post( $pic_atts[ 'caption' ] ) . '</p>'  ;
