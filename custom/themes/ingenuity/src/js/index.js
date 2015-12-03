2fe4837810f07d9e6dcbd9137e428f5377fee476
Emily Dela Cruz <emily@hypenotic.com>
2015-12-03 15:46:08 -0500

    Update: js google maps tweaks

--- custom/themes/ingenuity/src/js/index.js
+++ custom/themes/ingenuity/src/js/index.js
@@ -200,9 +200,19 @@ if ( $( "#contact-map" ).length ) {
        styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"administrative","elementType":"labels.text.stroke","stylers":[{"color":"#2d2d2d"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#2d2d2d"}]},{"featureType":"landscape","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"landscape","elementType":"labels.text.stroke","stylers":[{"color":"#2d2d2d"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#2d2d2d"}]},{"featureType":"poi","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","elementType":"labels.text.stroke","stylers":[{"color":"#2d2d2d"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#2d2d2d"}]},{"featureType":"road","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#a68d29"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#a68d29"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"visibility":"simplified"},{"color":"#a68d29"}]},{"featureType":"transit.station","elementType":"geometry","stylers":[{"color":"#2d2d2d"},{"lightness":9},{"visibility":"simplified"}]},{"featureType":"transit.station","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"transit.station.airport","elementType":"labels.text.stroke","stylers":[{"color":"#2d2d2d"}]},{"featureType":"water","elementType":"geometry","stylers":[{"saturation":-83},{"lightness":-51}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"color":"#2d2d2d"}]}]
    };
 
+   // Determine zoon and pan amounts based on isDraggable (mobile)
+   if (isDraggable == false) {
+    var zoomNum = 16;
+    var panNum = 80;
+   } else {
+    var zoomNum = 16;
+    var panNum = 90;
+   }
+   
+
    var mapOptions2 = {
        // How zoomed in you want the map to start at (always required)
-       zoom: 15,
+       zoom: zoomNum,
 
        scrollwheel:  false,
        draggable: isDraggable,
@@ -227,6 +237,7 @@ if ( $( "#contact-map" ).length ) {
    var mapM = new google.maps.Map(moveMap, mapOptions2);
    //move map down
    mapC.panBy(0, -120);
+   mapM.panBy(panNum, 0);
 
   var currentpage = window.location.href;
   if (currentpage == "http://hypelabs.ca/ingenuity/contact-us/") {
