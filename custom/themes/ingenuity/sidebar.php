d80121a230e67bd4825b2126dd28ba9482e76d08
Emily Dela Cruz <emily@hypenotic.com>
2015-12-09 16:04:45 -0500

    Update: tweaks

--- custom/themes/ingenuity/sidebar.php
+++ custom/themes/ingenuity/sidebar.php
@@ -16,10 +16,10 @@ $authors     = get_post_meta( $post->ID, '_authors_authorlist', true );
 		<?php } ?>
 		
 	</p>
-	<p><span class="bolded-text">Date:</span><br> <?php the_time( 'M j, Y' ); ?> </p>
-	<p><span class="bolded-text">Category:</span><br> <?php echo get_the_category_list(); ?></p>
+	<p><span class="bolded-text">Date:</span><br><?php the_time( 'M j, Y' ); ?> </p>
+	<p class="sidebar-cats"><span class="bolded-text">Category:</span><?php echo get_the_category_list(); ?></p>
 	<?php if (has_tag()) { ?>
-		<p><span class="bolded-text">Tags:</span><br>the_tags();</p>
+		<p class="sidebar-tags"><span class="bolded-text">Tags:</span><br><?php the_tags(); ?> </p>
 	<?php }  else {} ?>
 </aside>
 
