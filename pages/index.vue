3087108060c7f957bccbd82cc42fc29d976cfbf2
Hubert <hubhcsi@gmail.com>
2018-07-13 10:35:37 -0400

    change meta for index

--- pages/index.vue
+++ pages/index.vue
@@ -37,8 +37,8 @@
                     { hid: 'og:title', property: 'og:title', content: "Ingenuity" },
                     { hid: 'og:type', property: 'og:type', content: 'website' },
                     { hid: 'og:url', property: 'og:url', content: 'http://172.104.208.23/' },
-                    { hid: 'og:image', property: 'og:image', content: '/logo.svg' },
-                    { hid: 'description', name: 'description', content: 'Hello World Index Page!!!' }
+                    { hid: 'og:image', property: 'og:image', content: '/logo.png' },
+                    { hid: 'description', name: 'description', content: 'A Design Build & General Contracting team that believes in happiness, craft, productivity, detail, and creativity in each square foot.' }
                 ],
             }
         },
