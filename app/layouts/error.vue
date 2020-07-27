8d596e38a82abea393b9823d69a8c0aa9c3eb723
Hubert <hubhcsi@gmail.com>
2020-07-27 10:44:48 -0400

    iframe is not the play

--- app/layouts/error.vue
+++ app/layouts/error.vue
@@ -5,10 +5,9 @@
       <div class="video-overlay">
       </div>
       <div class="video">
-          <!-- <video playsinline autoplay muted loop id="bgvid">
-              <source src="/assets/videos/ingenuity_fast.mp4" type="video/mp4">
-          </video> -->
-          <iframe name="iframe1" id="iframe1" src="https://player.vimeo.com/video/442032431?autoplay=1&loop=1&autopause=0" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+          <video playsinline autoplay muted loop poster="https://ingenuity.ca/cms/custom/uploads/2018/02/poster.jpg" id="bgvid">
+              <source src="~/assets/videos/ingenuity_fast.mp4" type="video/mp4">
+          </video>
       </div>
       <div class="hgroup rw-wrapper" id="content-start">
         <h2 class="rw-sentence">
@@ -51,19 +50,14 @@ export default {
 
 <style lang="scss">
 
-  @import '~/assets/sass/variables.scss';
-  @import '~/assets/sass/global.scss';
-  @import '~/assets/sass/typography.scss';
-  @import '~/assets/sass/components/nav.scss';
-  @import '~/assets/sass/components/menu.scss';
-  @import '~/assets/sass/components/footer.scss';
-  @import '~/assets/sass/components/skip-to-content.scss';
-  @import '~/assets/sass/views/home.scss';
-
-  .video iframe{
-    width: 101%;
-    height: 101%;
-  }
+   @import '~/assets/sass/variables.scss';
+   @import '~/assets/sass/global.scss';
+   @import '~/assets/sass/typography.scss';
+   @import '~/assets/sass/components/nav.scss';
+   @import '~/assets/sass/components/menu.scss';
+   @import '~/assets/sass/components/footer.scss';
+   @import '~/assets/sass/components/skip-to-content.scss';
+   @import '~/assets/sass/views/home.scss';
 
 </style>
 
