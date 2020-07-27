b93d25eef7fe6cf584f196ab3a6d672b2dd501b7
Hubert <hubhcsi@gmail.com>
2020-07-27 10:10:28 -0400

    embed stopgap

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,9 +3,10 @@
         <div class="video-overlay">
         </div>
         <div class="video">
-            <video playsinline autoplay muted loop id="bgvid">
-                <source media="(max-width: 450px)" src="~/assets/videos/ingenuity_fast.mp4" type="video/mp4">
-            </video>
+            <!-- <video playsinline autoplay muted loop id="bgvid">
+                <source src="/assets/videos/ingenuity_fast.mp4" type="video/mp4">
+            </video> -->
+            <iframe name="iframe1" id="iframe1" src="https://player.vimeo.com/video/442032431?autoplay=1&loop=1&autopause=0" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
         </div>
         <div class="hgroup rw-wrapper" id="content-start">
             <h2 class="rw-sentence">
@@ -59,4 +60,9 @@
 <style lang="scss" scoped>
     @import '~/assets/sass/variables.scss';
     @import '~/assets/sass/views/home.scss';
+
+    .video iframe{
+        width: 101%;
+        height: 101%;
+    }
 </style>
